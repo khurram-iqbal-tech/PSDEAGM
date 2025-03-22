4920a9d20cca4030e2bc4b997126fc1bbe4c3275
khurram-iqbal-tech <khurram.ms1@gmail.com>
2025-03-22 14:23:05 +0500

    Initial Commit

---
+++ PSDEAGM/PSDEAGMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSDEAGMApp.swift
+//  PSDEAGM
+//
+//  Created by MacBook Pro on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PSDEAGMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
